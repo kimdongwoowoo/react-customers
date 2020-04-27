06a323c756645d76ef15f77399677e031871c363
김동우/연구원/ID SST개발Project(woodong.kim@lge.com) <woodong.kim@lge.com>
2020-04-27 16:09:21 +0900

    material-ui 테이블 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Customer from './Components/Customer'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 const customers = [
   {
     'id': 1,
@@ -13,8 +18,8 @@ const customers = [
   {
     'id': 2,
     'image': '/images/호랑이2.jpg',
-    'name': '나동빈',
-    'birthday': '960508',
+    'name': '김동우',
+    'birthday': '911012',
     'gender': '남자',
     'job': '프로그래머'
   },
@@ -34,12 +39,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        {
-          customers.map(c => {
-          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-          } )
-        }
-
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+            customers.map(c => {
+              return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            })
+          }
+          </TableBody>
+        </Table>
       </div>
     );
   }

04cfdada8ad6f2d2cf6ac4d770a41ba781ec803f
김동우/연구원/ID SST개발Project(woodong.kim@lge.com) <woodong.kim@lge.com>
2020-04-27 14:39:42 +0900

    jpg추가 Customer(Info+Profile) 컴포넌트 생성 App.js에 데이터 추가, map으로 Customer 컴포넌트 리턴

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import React, { Component } from 'react';
-class App extends Component() {
+import './App.css';
+import Customer from './Components/Customer'
+const customers = [
+  {
+    'id': 1,
+    'image': '/images/호랑이.jpg',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': '/images/호랑이2.jpg',
+    'name': '나동빈',
+    'birthday': '960508',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': '/images/호랑이3.jpg',
+    'name': '이순신',
+    'birthday': '961127',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]
+
+
+
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
+      <div>
+        {
+          customers.map(c => {
+          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+          } )
+        }
+
       </div>
     );
   }
